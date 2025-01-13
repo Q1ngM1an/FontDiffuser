cd4efeb5850dfc9200c838c3b7dbca3a14bd5eb8
qingmian <qingmiannn@163.com>
2025-01-13 17:23:59 +0800

    初始化提交测试

--- scripts/train_phase_1.sh
+++ scripts/train_phase_1.sh
@@ -14,8 +14,8 @@ accelerate launch train.py \
     --train_batch_size=16 \
     --perceptual_coefficient=0.01 \
     --offset_coefficient=0.5 \
-    --max_train_steps=440000 \
-    --ckpt_interval=40000 \
+    --max_train_steps=40000 \
+    --ckpt_interval=4000 \
     --gradient_accumulation_steps=1 \
     --log_interval=50 \
     --learning_rate=1e-4 \
